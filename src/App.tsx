9fcab15978e5ef7aa3eb36b6b2dbc8aa57b6d2a9
YasminTrembulack <yasmin.agostinho.012@gmail.com>
2026-02-10 16:03:36 -0300

    feat: create Footer component

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Header } from "./components/Header/Header";
 import { Home } from "./sections/Home/Home";
 import { About } from "./sections/About/About";
 import { GlobalStyle } from "./styles/global";
+import { Footer } from "./components/Footer/Footer";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Home />
         <About />
       </main>
+      <Footer />
     </>
   );
 }
