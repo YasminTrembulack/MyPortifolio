b031bdcfd8b55845fe482d663ed45d2654abccb6
YasminTrembulack <yasmin.agostinho.012@gmail.com>
2026-02-10 16:24:05 -0300

    feat: add email icon

--- src/components/Socials/Socials.tsx
+++ src/components/Socials/Socials.tsx
@@ -2,6 +2,7 @@ import { Container } from "./Socials.styles";
 
 import GithubIcon from "../../assets/icons/github.svg?react";
 import LinkedinIcon from "../../assets/icons/linkedin.svg?react";
+import EmailIcon from "../../assets/icons/email.svg?react";
 
 type Props = {
   $color?: string;
@@ -11,11 +12,14 @@ type Props = {
 export function Socials({ ...props }: Props) {
   return (
     <Container {...props}>
+      <a href="https://www.linkedin.com/in/yasmintrembulack/" target="_blank">
+        <LinkedinIcon />
+      </a>
       <a href="https://github.com/YasminTrembulack" target="_blank">
         <GithubIcon />
       </a>
-      <a href="https://www.linkedin.com/in/yasmintrembulack/" target="_blank">
-        <LinkedinIcon />
+      <a href="mailto:yasmin.agostinho.012@gmail.com" target="_blank">
+        <EmailIcon />
       </a>
     </Container>
   );
