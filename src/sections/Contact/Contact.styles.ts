dfb0a309b7a7bff6f25a2420ae3984e153df0872
YasminTrembulack <yasmin.agostinho.012@gmail.com>
2026-05-21 14:26:35 -0300

    feat: update contact section

--- src/sections/Contact/Contact.styles.ts
+++ src/sections/Contact/Contact.styles.ts
@@ -6,15 +6,45 @@ export const Section = styled.section`
   padding-bottom: 2rem;
 
   display: flex;
+  flex-direction:column;
   align-items: center;
   justify-content:center;
 
   background: ${({ theme }) => theme.colors.background};
   color: ${({ theme }) => theme.colors.softWhite};
+
+  margin-bottom: 50px;
+`;
+
+export const Header = styled.div<{ $visible: boolean }>`
+  display: flex;
+  flex-direction: column;
+  align-items: start;
+  justify-content: center;
+  width:100%;
+
+  opacity: 0;
+  transform: translateY(32px);
+  transition:
+    opacity 1.2s ease,
+    transform 1.2s ease;
+
+  ${({ $visible }) =>
+    $visible &&
+    `
+      opacity: 1;
+      transform: translateY(0);
+    `}
 `;
 
 export const Container = styled.div`
-  max-width: 1100px;
+  max-width: 1200px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const Content = styled.div`
   width: 100%;
 
   display: grid;
@@ -30,6 +60,7 @@ export const Container = styled.div`
 export const ContentHeader = styled.div`
   display: flex;
   justify-content: start;
+  align-items: center;
   gap: 0.75rem;
 
   @media (max-width: 900px) {
@@ -71,22 +102,20 @@ export const Title = styled.h2`
   color: ${({ theme }) => theme.colors.white};
 `;
 
-export const Text = styled.p`
-  font-size: 1.1rem;
-  line-height: 1.7;
-  color: ${({ theme }) => theme.colors.softWhite};
-  font-family: ${({ theme }) => theme.fonts.terciary};
-  font-weight: regular;
-`;
 
 export const Subtitle = styled.p`
-  max-width: 520px;
-  font-size: 0.95rem;
-  line-height: 1.6;
-  color: ${({ theme }) => theme.colors.softWhite};
-  opacity: 0.65;
+  margin-top: 0.75rem;
+  color: ${({ theme }) => theme.colors.greyLight};
+  text-align: center;
 `;
 
+export const Divider = styled.div`
+  width: 100%;
+  height: 1px;
+  background: ${({ theme }) => theme.colors.greyDark  };
+  opacity: 0.6;
+  margin: 24px 0;
+`;
 
 export const InfoList = styled.div`
   display: flex;
@@ -135,6 +164,8 @@ export const InfoItem = styled.div<{ $visible: boolean }>`
 
 export const InfoTitle = styled.h4`
   font-weight: 600;
+  color: ${({ theme }) => theme.colors.softWhite};
+
 `;
 
 export const InfoText = styled.p`
@@ -154,43 +185,54 @@ export const Label = styled.label`
   font-size: 14px;
   font-weight: 600;
   margin-bottom: 8px;
+  color: ${({ theme }) => theme.colors.softWhite};
+
 `;
 
 export const Input = styled.input`
   width: 100%;
-  border: 1px solid #d1d5db;
+  background: ${({ theme }) => theme.colors.background};
+  color: ${({ theme }) => theme.colors.softWhite};
+  border: 1px solid ${({ theme }) => theme.colors.grey};
   border-radius: 6px;
   padding: 12px 16px;
   outline: none;
 
   &:focus {
-    box-shadow: 0 0 0 2px #6d28d9;
+    box-shadow: 0 0 0 2px ${({ theme }) => theme.colors.bluePrimary};
   }
 `;
 
 export const Textarea = styled.textarea`
   width: 100%;
-  border: 1px solid #d1d5db;
+  border: 1px solid ${({ theme }) => theme.colors.grey};
+  background: ${({ theme }) => theme.colors.background};
+  color: ${({ theme }) => theme.colors.softWhite};
+
   border-radius: 6px;
   padding: 12px 16px;
   resize: none;
   outline: none;
 
   &:focus {
-    box-shadow: 0 0 0 2px #6d28d9;
+    box-shadow: 0 0 0 2px ${({ theme }) => theme.colors.bluePrimary};
   }
 `;
 
 export const Button = styled.button`
-  background: #3b0086;
-  color: white;
-  padding: 12px 24px;
-  border-radius: 6px;
-  border: none;
-  cursor: pointer;
-  transition: 0.2s;
+  padding: 0.8rem 2rem;
+  border-radius: 999px;
+  font-weight: 600;
+  text-decoration: none;
+  transition: all 0.3s ease;
+
+  background:  ${({ theme }) => theme.colors.background};
+  color: ${({ theme }) => theme.colors.softWhite};
+  border: 2px solid ${({ theme }) => theme.colors.yellowAccent};
 
   &:hover {
-    background: #2f006b;
+    transform: translateY(-4px);
+    background: ${({ theme }) => theme.colors.yellowAccent};
+    color: ${({ theme }) => theme.colors.softBlack};
   }
 `;
--- src/sections/Contact/Contact.tsx
+++ src/sections/Contact/Contact.tsx
@@ -10,13 +10,16 @@ import {
     Input,
     Label,
     Section,
-    Text,
     Textarea,
     Title,
     InfoTitle,
     InfoText,
     Right,
-    Left
+    Left,
+    Subtitle,
+    Header,
+    Divider,
+    Content
 } from "./Contact.styles";
 import { flower } from "../../assets/ascii-art";
 import LinkedinIcon from "../../assets/icons/linkedin.svg?react";
@@ -68,55 +71,53 @@ export function Contact() {
     return (
         <Section id="contact" ref={ref}>
             <Container>
-
-                <Left>
+                <Header $visible={isVisible}>
                     <ContentHeader>
                         <Icon>{flower}</Icon>
                         <Title>Get in Touch</Title>
                     </ContentHeader>
+                    <Subtitle>
+                        Have a project, collaboration idea, or opportunity in mind? I’m always open to new connections and would love to hear from you.
+                    </Subtitle>
+                </Header>
+                <Divider />
+                <Content>
+                    <Left>
+                        <InfoList>
+                            {infoItems.map((item) => (
+                                <InfoItem key={item.title} $visible={isVisible}>
+                                    <a href={item.link} target="_blank" rel="noreferrer">
+                                        {item.icon}
+                                    </a>
 
-                    <Text>
-                        Have a project in mind, a collaboration opportunity, or just want to connect? I’d love to hear from you.
-                    </Text>
+                                    <div>
+                                        <InfoTitle>{item.title}</InfoTitle>
+                                        <InfoText>{item.text}</InfoText>
+                                    </div>
+                                </InfoItem>
+                            ))}
+                        </InfoList>
+                    </Left>
+                    <Right>
+                        <Form>
+                            <Field>
+                                <Label>What should I call you?</Label>
+                                <Input type="text" placeholder="John Doe" />
+                            </Field>
 
-                    <InfoList>
-                        {infoItems.map((item) => (
-                            <InfoItem key={item.title} $visible={isVisible}>
-                                <a href={item.link} target="_blank" rel="noreferrer">
-                                    {item.icon}
-                                </a>
+                            <Field>
+                                <Label>Email</Label>
+                                <Input type="email" placeholder="johndoe@example.com" />
+                            </Field>
 
-                                <div>
-                                    <InfoTitle>{item.title}</InfoTitle>
-                                    <InfoText>{item.text}</InfoText>
-                                </div>
-                            </InfoItem>
-                        ))}
-                    </InfoList>
-                </Left>
-
-                {/* Right Side */}
-                <Right>
-                    <Form>
-                        <Field>
-                            <Label>What should I call you?</Label>
-                            <Input type="text" placeholder="John Doe" />
-                        </Field>
-
-                        <Field>
-                            <Label>Email</Label>
-                            <Input type="email" placeholder="johndoe@example.com" />
-                        </Field>
-
-                        <Field>
-                            <Label>Tell me about your idea</Label>
-                            <Textarea rows={5} placeholder="Your Message here..." />
-                        </Field>
-
-                        <Button type="submit">Send Message</Button>
-                    </Form>
-                </Right>
-
+                            <Field>
+                                <Label>Tell me about your idea</Label>
+                                <Textarea rows={5} placeholder="Your Message here..." />
+                            </Field>
+                            <Button type="submit">Send Message</Button>
+                        </Form>
+                    </Right>
+                </Content>
             </Container>
         </Section>
     );
