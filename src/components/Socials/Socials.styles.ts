5752b253b26bfac5f94af0c6b0d8eb92a7cb2f83
YasminTrembulack <yasmin.agostinho.012@gmail.com>
2026-05-25 19:39:23 -0300

    feat: improve Socials component

--- src/components/Socials/Socials.styles.ts
+++ src/components/Socials/Socials.styles.ts
@@ -1,30 +1,78 @@
 import styled from "styled-components";
+import { fadeLeft, fadeRight, fadeUp, type AnimationProps } from "../../styles/transitions";
 
-export const Container = styled.div<{
-  $color?: string;
-  $hover?: string;
-}>`
+export type InfoListProps = {
+  $flexDirection?: "row" | "column";
+  $gap?: number;
+}
+
+export type InfoItemProps = {
+  $variant?: "blue" | "grey"; 
+}
+
+export type InfoRevealProps = AnimationProps & {
+  $fade?: "up" | "left" | "right";
+}
+
+export const InfoList = styled.div<InfoListProps>`
   display: flex;
-  gap: 0.8rem;
+  flex-direction: ${props => props.$flexDirection};
+  gap: ${props => props.$gap}rem;
 
-  a {
-    color: ${({ $color, theme }) => $color || theme.colors.blueSoft};
+`;
 
-    transition:
-      opacity 0.2s,
-      color 0.2s;
+export const InfoReveal = styled.div<InfoRevealProps>`
+  ${({ $fade }) => {
+    if ($fade === "up") return fadeUp;
+    if ($fade === "left") return fadeLeft;
+    if ($fade === "right") return fadeRight;
 
-    svg {
-      width: 24px;
-      height: 24px;
-    }
+    return "";
+  }};
+`;
 
-    &:hover {
-      color: ${({ $hover, theme }) => $hover || theme.colors.bluePrimary};
-    }
+export const Link = styled.a`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  text-decoration: none;
+  gap: 16px;
+`;
+ 
+export const Content = styled.div``;
+
+export const InfoItem = styled.div<InfoItemProps>`
+  display: flex;
+  align-items: center;
+  cursor: pointer;
+
+  transition:
+    transform 0.3s ease,
+    border-color 0.3s ease,
+    box-shadow 0.3s ease;
+
+  svg {
+    width: 24px;
+    height: 24px;
+    color: ${({ theme, $variant }) => 
+      $variant === "grey" ? theme.colors.greyLight : theme.colors.blueSoft};
   }
 
-  @media (max-width: 900px) {
-    justify-content: center;
+  &:hover svg {
+    color: ${({ theme, $variant }) => $variant === "grey" ? theme.colors.softBlack : theme.colors.bluePrimary};
   }
+
+  &:hover{
+    transform: translateY(-2px);
+  }
+`;
+
+export const InfoTitle = styled.h4`
+  font-weight: 600;
+  color: ${({ theme }) => theme.colors.softWhite};
+
+`;
+
+export const InfoText = styled.p`
+  color: ${({ theme }) => theme.colors.grey};
 `;
--- src/components/Socials/Socials.tsx
+++ src/components/Socials/Socials.tsx
@@ -1,26 +1,56 @@
-import { Container } from "./Socials.styles";
+import type { HTMLAttributes } from "react";
 
-import GithubIcon from "../../assets/icons/github.svg?react";
-import LinkedinIcon from "../../assets/icons/linkedin.svg?react";
-// import EmailIcon from "../../assets/icons/email.svg?react";
+import * as S from "./Socials.styles";
+import type { AnimationProps } from "../../styles/transitions";
+import { getContacts } from "../../data/contacts";
+import { useTranslation } from "react-i18next";
 
-type Props = {
-  $color?: string;
-  $hover?: string;
-};
-
-export default function Socials({ ...props }: Props) {
-  return (
-    <Container {...props}>
-      <a href="https://www.linkedin.com/in/yasmintrembulack/" target="_blank">
-        <LinkedinIcon />
-      </a>
-      <a href="https://github.com/YasminTrembulack" target="_blank">
-        <GithubIcon />
-      </a>
-      {/* <a href="mailto:yasmin.agostinho.012@gmail.com" target="_blank">
-        <EmailIcon />
-      </a> */}
-    </Container>
-  );
+interface SocialsProps extends HTMLAttributes<HTMLDivElement> {
+    contacts: ("email" | "linkedin" | "github" | "location")[];
+    showTitle?: boolean;
+    showText?: boolean;
 }
+
+function Socials({
+    contacts,
+    showTitle = false,
+    showText = false,
+    $variant = "blue",
+    $flexDirection = "column",
+    $visible = true,
+    $gap = 0,
+    $delay,
+    $fade,
+    ...rest
+}: SocialsProps 
+    & AnimationProps 
+    & S.InfoItemProps 
+    & S.InfoListProps 
+    & S.InfoRevealProps
+) {
+    const { t } = useTranslation();
+
+    return (
+        <S.InfoList $flexDirection={$flexDirection} $gap={$gap} {...rest}>
+            {contacts.map((c, index) => {
+                const contact = getContacts(t)[c];
+
+                return (
+                    <S.InfoReveal key={index} $visible={$visible} $delay={index * 120} $fade={$fade}>
+                        <S.InfoItem $variant={$variant}>
+                            <S.Link href={contact.link} target="_blank" rel="noreferrer">
+                                {contact.icon}
+                                <S.Content>
+                                    {showTitle && <S.InfoTitle>{contact.title}</S.InfoTitle>}
+                                    {showText && <S.InfoText>{contact.text}</S.InfoText>}
+                                </S.Content>
+                            </S.Link>
+                        </S.InfoItem>
+                    </S.InfoReveal>
+                )
+            })}
+        </S.InfoList>
+    );
+}
+
+export default Socials;
