4e098c12af7924aaad6046a291ab1637f2f4fba7
YasminTrembulack <yasmin.agostinho.012@gmail.com>
2026-05-25 19:39:47 -0300

    feat: update getContacts type

--- src/data/contacts.tsx
+++ src/data/contacts.tsx
@@ -11,32 +11,32 @@ export type Contacts = {
     text: string;
 };
 
-export const getContacts = (t: TFunction): Contacts[] => [
-  {
+export const getContacts = (t: TFunction): Record<string, Contacts> => ({
+  "email": {
     link: "mailto:yasmin.agostinho.012@gmail.com",
     icon: <GmailIcon />,
     title: t("contact.cards.email.title"),
     text: t("contact.cards.email.text"),
   },
 
-  {
+  "linkedin": {
     link: "https://www.linkedin.com/in/yasmintrembulack",
     icon: <LinkedinIcon />,
     title: t("contact.cards.linkedin.title"),
     text: t("contact.cards.linkedin.text"),
   },
 
-  {
+  "github": {
     link: "https://github.com/yasmintrembulack",
     icon: <GithubIcon />,
     title: t("contact.cards.github.title"),
     text: t("contact.cards.github.text"),
   },
 
-  {
+  "location": {
     link: "https://maps.app.goo.gl/LG8iyi9HdagjTzQr8",
     icon: <Location />,
     title: t("contact.cards.location.title"),
     text: t("contact.cards.location.text"),
   },
-];
+});
