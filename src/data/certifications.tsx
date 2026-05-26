f8caa1328d497f357ea116c2cfba9719c8f897a2
YasminTrembulack <yasmin.agostinho.012@gmail.com>
2026-05-26 12:08:00 -0300

    feat: add temp description to certifications

--- src/data/certifications.tsx
+++ src/data/certifications.tsx
@@ -2,22 +2,26 @@ export type Certification = {
   title: string;
   institution: string;
   year: string;
+  description: string;
 };
 
 export const certifications: Certification[] = [
   {
     title: "Técnico Desenvolvimento de Sistemas",
     institution: "Robert Bosch - Curitiba, PR - Brazil.",
-    year: "2024"
+    year: "2024",
+    description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facilis, natus? Laboriosam temporibus inventore, eos odit eveniet culpa consequuntur. Qui quae aut natus animi vitae iste. Ut quis sapiente quidem et?"
   },
   {
     title: "Técnico Desenvolvimento de Sistemas",
     institution: "SENAI, Dr. Celso Charuri - Curitiba, PR - Brazil.",
-    year: "2024"
+    year: "2024",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet alias accusantium doloribus possimus aut, magni ad error, nostrum cum consequatur dignissimos aspernatur, qui provident consequuntur quisquam facilis tempora quibusdam? Reprehenderit."
   },
   {
     title: "Bachelor of Software Engineering",
     institution: "UniSenai - São José dos Pinhais, PR - Brazil.",
-    year: "Expected 2027"
-  },
+    year: "Expected 2027",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit suscipit velit, magni deleniti enim neque sint tenetur, quibusdam et, atque nisi laboriosam rerum veritatis error incidunt vero! Dolorum, non velit!"
+  }
 ];
