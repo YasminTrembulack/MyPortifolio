e69cea2810e0cbede31cb80fd22264d354679802
YasminTrembulack <yasmin.agostinho.012@gmail.com>
2026-05-27 10:48:01 -0300

    feat: update folder

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,8 +1,8 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 
-import en from "../locales/en/common.json";
-import pt from "../locales/pt/common.json";
+import en from "./locales/en/common.json";
+import pt from "./locales/pt/common.json";
 
 i18n.use(initReactI18next).init({
   resources: {
